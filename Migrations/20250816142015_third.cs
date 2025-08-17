e771aed609ac86cf34690617910319de4ac40a5b
Srikanth <Srikanthsrii9390@gmail.com>
2025-08-17 14:56:21 +0530

    Added

---
+++ Migrations/20250816142015_third.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace employer_employee.Migrations
+{
+    /// <inheritdoc />
+    public partial class third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employers_EmployerId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployerId",
+                table: "Employees",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "Employers",
+                columns: new[] { "EmployerId", "Email", "Name", "PasswordHash" },
+                values: new object[] { 1, "admin@company.com", "Admin", "Srik@NTh" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employers_EmployerId",
+                table: "Employees",
+                column: "EmployerId",
+                principalTable: "Employers",
+                principalColumn: "EmployerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employers_EmployerId",
+                table: "Employees");
+
+            migrationBuilder.DeleteData(
+                table: "Employers",
+                keyColumn: "EmployerId",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployerId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employers_EmployerId",
+                table: "Employees",
+                column: "EmployerId",
+                principalTable: "Employers",
+                principalColumn: "EmployerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Migrations/20250816160801_sixth.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace employer_employee.Migrations
+{
+    /// <inheritdoc />
+    public partial class sixth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DailyWage",
+                table: "Employees",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailyWage",
+                table: "Employees");
+        }
+    }
+}
